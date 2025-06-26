fcbbd0d7ececafd2280c3c46ab6e4d7fc8747865
Ali Aytekin <aliraytekin@gmail.com>
2025-06-26 19:52:26 +0300

    Search bar

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,12 +1,20 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
   validates :title, presence: true
   validates :description, presence: true
   validates :address, presence: true
-  validates :price_per_night, presence: true
-  validates :number_of_beds, :number_of_bathrooms, presence: true
+  validates :price_per_night, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :number_of_bathrooms, :number_of_beds, :guests_limit, numericality: { only_integer: true }
 
   PROPERTY_TYPES = ["Apartment", "House", "Studio", "Villa", "Cabin"]
   validates :property_type, inclusion: { in: PROPERTY_TYPES }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_offers,
+                  against: %i[title description],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
