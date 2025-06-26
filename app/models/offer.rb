e6c3e5b81c4b056e25a4a3a1dbdaf59d4f531ae8
Ali Aytekin <aliraytekin@gmail.com>
2025-06-26 11:13:11 +0300

    fix: change code

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,12 +1,12 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :title, presence:true
-  validates :description, presence:true
-  validates :address, presence:true
-  validates :price_per_night, presence:true
-  validates :number_of_beds, :number_of_bathrooms, presence:true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :price_per_night, presence: true
+  validates :number_of_beds, :number_of_bathrooms, presence: true
 
   PROPERTY_TYPES = ["Apartment", "House", "Studio", "Villa", "Cabin"]
-  validates :property_type, inclusion: {in: PROPERTY_TYPES }
+  validates :property_type, inclusion: { in: PROPERTY_TYPES }
 end
