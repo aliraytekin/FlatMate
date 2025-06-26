adf3776a6dde1bc3845baef77f7fed01ea19c844
Abiola Adeboye <louisdabee@gmail.com>
2025-06-26 16:19:06 +0200

    add partial form

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,11 +1,12 @@
 class OffersController < ApplicationController
-  before_action :set_offer, only: %i[show create update destroy]
+  before_action :set_offer, only: %i[show create update destroy edit]
 
   def index
     @offers = Offer.all
   end
 
   def show
+    @offer = Offer.find(params[:id])
   end
 
   def new
