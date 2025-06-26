bc5621e1145a234f660c08de6a922ee365653f38
Aref Lameche <lameche.aref94@gmail.com>
2025-06-26 06:33:05 +0200

    show page Aref

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,8 +1,10 @@
 class OffersController < ApplicationController
   def index
+    @offer = Offer.all
   end
 
   def show
+    @offer = Offer.find(params[:id])
   end
 
   def new
@@ -18,5 +20,11 @@ class OffersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_params
+    params.require(:offer).permit(:title, :description, :address, :price_per_night, :number_of_beds, :number_of_bathrooms)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  get 'offers/index'
-  get 'offers/show'
-  get 'offers/new'
-  get 'offers/create'
-  get 'offers/edit'
-  get 'offers/update'
-  get 'offers/destroy'
-  devise_for :users
+  # get 'offers/index'
+  # get 'offers/show'
+  # get 'offers/new'
+  # get 'offers/create'
+  # get 'offers/edit'
+  # get 'offers/update'
+  # get 'offers/destroy'
+
   root to: "pages#home"
+
+  resources :offers, only: [:index, :show, :edit]
 
   resources :offers do
     resources :bookings, only: [:new, :create]
   end
+
   resources :bookings, only: [:index, :show, :edit, :update]
-  
+
+  devise_for :users
+
 end
