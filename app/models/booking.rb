dc02e28ac3ee2e7b816ceed927f36567f287d16d
Alain Nassar <nassaralain7@gmail.com>
2025-06-28 00:18:36 +0200

    fixed the enum status issue

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :offer
   validates :start_date, :end_date, :number_of_guests, presence: true
   validates :number_of_guests, numericality: { only_integer: true, greater_than: 0 }
-
+  attribute :status, :integer
   enum status: { pending: 0, processing_payment: 1, confirmed: 2, cancelled: -1 }
 end
