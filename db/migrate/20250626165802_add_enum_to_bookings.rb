5d0ddf03fc710b9cf37a5ead6d9ee1b34a5cbb30
Ali Aytekin <aliraytekin@gmail.com>
2025-07-01 11:53:28 +0300

    Status fixed

--- db/migrate/20250626165802_add_enum_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddEnumToBookings < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookings, :status, :integer, default: 0, null: false
-  end
-end
--- db/migrate/20250701085157_add_status_to_bookings.rb
+++ db/migrate/20250701085157_add_status_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddStatusToBookings < ActiveRecord::Migration[7.1]
   def change
-    add_column :bookings, :status, :string, default: "active"
+    add_column :bookings, :status, :integer, default: 0, null: false
   end
 end
