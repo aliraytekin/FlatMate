c0a085ff35464f710f708e56f53a077f75631857
Ali Aytekin <aliraytekin@gmail.com>
2025-07-01 21:54:27 +0300

    Heroku fix

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,6 +3,8 @@ class Offer < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many_attached :photos
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :title, presence: true
   validates :description, presence: true
