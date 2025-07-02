84b25dc7242883c564de37068047114db148c9ad
Abiola Adeboye <louisdabee@gmail.com>
2025-07-02 14:37:08 +0200

    save schema

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_01_102623) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status", default: "active"
+    t.integer "status"
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
