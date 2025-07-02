f856e46bfd68b08bf64e34f402cfbeebfce5277d
Alona Shautdinova <alenahtc25@gmail.com>
2025-07-02 11:19:28 +0200

    added review display on offer page and delete and edit function, fixed the avaibilaty issue

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,10 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :offer
 
-  validates :rating, presence: true, inclusion: { in: 1..5 }
-  validates :comment, presence: true
+  validates :rating,
+            inclusion: { in: 1..5, message: "must be a number between 1 and 5" }
+
+  validates :comment,
+            presence: true,
+            length: { maximum: 300, message: "is too long (maximum is 300 characters)" }
 end
