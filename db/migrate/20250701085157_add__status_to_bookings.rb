c966e2629699a1d1368ecdf3cefda4d994f5f607
Ali Aytekin <aliraytekin@gmail.com>
2025-07-05 12:09:03 +0300

    Heroku

--- db/migrate/20250701085157_add__status_to_bookings.rb
+++ db/migrate/20250701085157_add__status_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddStatusToBookings < ActiveRecord::Migration[7.1]
   def change
-    add_column :bookings, :status, :integer, null: false
+    add_column :bookings, :status, :integer, null: false, default: 0
   end
 end
